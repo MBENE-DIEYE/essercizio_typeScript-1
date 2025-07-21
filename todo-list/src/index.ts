460583a211042a7a8e95e81f5dcbfb8133839d43
mbene dieye <mbenedieye757@gmail.com>
2025-07-21 18:55:54 +0200

    addTodo

---
+++ todo-list/src/index.ts
@@ -0,0 +1,14 @@
+import { Todo } from "./type";
+
+let todos: Todo[]
+
+const addTodo = (title: string) => {
+    return {
+        Todo: {
+            id: 1,
+            completo: false
+        }
+
+    }
+}
+addTodo
