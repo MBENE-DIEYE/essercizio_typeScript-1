c5188f3735655176cdbb8471de753fa1d3b5a7fb
mbene dieye <mbenedieye757@gmail.com>
2025-07-22 10:53:00 +0200

    typeScript

--- todo-list/src/index.ts
+++ todo-list/src/index.ts
@@ -1,14 +1,68 @@
-import { Todo } from "./type";
+import { Todo, user } from "./type";
 
-let todos: Todo[]
 
-const addTodo = (title: string) => {
-    return {
-        Todo: {
-            id: 1,
-            completo: false
-        }
+let todos: Todo[] = []
 
+function addTodo(title: string, metaData?: any | string | object): Todo {
+    const newTodo: Todo = {
+        id: 1,
+        title: "",
+        completo: false,
+        metaData: ""
+    };
+
+    todos.push(newTodo);
+    return newTodo;
+}
+addTodo("compra il latte")
+
+const assignTodoToUser = (todoId: number, userId: number) => {
+    const todo = todos.find(t => t.id === todoId)
+    if (todo) {
+        todo.userId = userId
+        return true
+    }
+    console.error(`Todo con id ${todoId} non trovato`)
+    return false
+}
+assignTodoToUser
+
+const getUserTodo = (userId: number) => {
+    const todo = todos.filter(t => t.id === userId)
+    return todo
+}
+getUserTodo
+
+const throwError = (message?: string): never => {
+    throw new Error(message)
+}
+throwError
+
+const parseInput = (input: unknown) => {
+    if (typeof input === "string") {
+        return input
+    }
+    else if (typeof input === "number") {
+        return "input"
+    }
+    else {
+        throw new Error(throwError())
     }
 }
-addTodo
+parseInput
+
+const updateTodo = () => {
+    const updata: Partial<Todo> = {
+        id: 1
+    }
+}
+
+updateTodo
+
+const getTodoSummary = () => {
+    type TupleArray = [String, Boolean]
+    const array: TupleArray = ["tilte", false]
+
+    return array
+}
+getTodoSummary
--- todo-list/src/type.ts
+++ todo-list/src/type.ts
@@ -2,5 +2,18 @@ export interface Todo {
     id: number,
     title: string,
     completo: boolean
+    userId?: number,
+    metaData: any
 
+}
+
+export interface user {
+    id: number,
+    name: string,
+    email?: string
+    readonly todos: readonly Todo[]
+}
+
+export interface TodoWithMetaData extends Todo {
+    metaData: Date
 }
